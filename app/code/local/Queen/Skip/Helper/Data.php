c6644cfc3affa77191cb96fb3ad4257a8017a27d
Quyen Dam <quyendam.2612@gmail.com>
2015-06-19 11:59:22 +0700

    queen ex

---
+++ app/code/local/Queen/Skip/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/18/15
+ * Time: 8:19 PM
+ */ 
+class Queen_Skip_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ app/code/local/Queen/Skip/sql/queen_skip_setup/mysql4-install-0.1.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 6/18/15
+ * Time: 8:19 PM
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
