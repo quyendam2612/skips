ab907fa744e91807d3dbb3a821e9cf5f48e12a1b
Quyen Dam <quyendam.2612@gmail.com>
2015-07-15 08:48:44 +0700

    refactory code

--- app/code/community/Emipro/Customoptions/controllers/Adminhtml/IndexController.php
+++ app/code/community/Emipro/Customoptions/controllers/Adminhtml/IndexController.php
@@ -10,63 +10,19 @@
  * //////////////////////////////////////////////////////////////////////////////////////
  */
 ini_set("max_execution_time", 0);
-class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action {
 
-    public function indexAction() {
-    
+class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+
         $this->loadLayout();
         $this->renderLayout();
     }
-	public function saveSkuAction()
-	{
-		$feature_id = Mage::getModel('catalog/product')->getIdBySku('customoptionmaster');
 
-        $optionId = $this->getRequest()->getParam("id");
-        $sku = Mage::getModel("catalog/product_option")->load($optionId)->getData("sku");
-
-        $sql = "select product_id from " . Mage::getConfig()->getTablePrefix() . "catalog_product_option where sku='$sku' and product_id != " . $feature_id;
-        $results = Mage::getSingleton('core/resource')->getConnection('core_read')->fetchAll($sql);
-        $categoryid = array();
-        $data = $this->getRequest()->getPost();
-
-		$skurmv=str_replace(array("\r\n","\r"),",",$data["removesku"]["remove_sku"]);
-		$skurmvarray=explode(",",$skurmv);
-		     
-        if(count($skurmvarray)>0 && !empty($skurmvarray[0]))
-        {
-			   
-			try{
-			$optionModel = Mage::getModel("customoptions/customoptionscollection");
-				foreach($skurmvarray as $rmsku)
-				{
-						
-						if(!empty($rmsku))
-						{
-							$product = Mage::getModel('catalog/product');
-							$productId = $product->getIdBySku(trim($rmsku));
-							if(!empty($productId))
-							{
-								$optionModel->deleteCustomOption($sku, $productId);
-							}
-						}
-				}
-		
-				Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('customoptions')->__('Option was successfully saved'));
-				Mage::getSingleton('adminhtml/session')->setFormData(false);
-				$this->_redirect('*/*/');
-				return;
-			}
-			catch (Exception $e) {
-            Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-            Mage::getSingleton('adminhtml/session')->setFormData($data);
-            $this->_redirect('*/*/adminhtml_remove/edit', array('id' => $this->getRequest()->getParam('id')));
-            return;
-				}
-		}
-		$this->_redirect('*/*/editsku', array('id' => $this->getRequest()->getParam('id')));
-            return;
-	}	
-    public function saveAction() {
+    public function saveSkuAction()
+    {
         $feature_id = Mage::getModel('catalog/product')->getIdBySku('customoptionmaster');
 
         $optionId = $this->getRequest()->getParam("id");
@@ -76,55 +32,93 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         $results = Mage::getSingleton('core/resource')->getConnection('core_read')->fetchAll($sql);
         $categoryid = array();
         $data = $this->getRequest()->getPost();
-        
-		if(isset($data["productsku"]))
-		{
-		
-					$skuvalue=str_replace(array("\r\n","\r"),",",$data["productsku"]["sku_list"]);
-					$skuarray=explode(",",$skuvalue);
-					if(count($skuarray)>0 && !empty($skuarray[0]))
-					{
-						try
-						{		
-						$optionModel = Mage::getModel("customoptions/customoptionscollection");
-						foreach($skuarray as $prodsku)
-						{
-								if(!empty($prodsku))
-								{
-									$product = Mage::getModel('catalog/product');
-									$productId = $product->getIdBySku(trim($prodsku));
-									if(!empty($productId))
-									{
-										$optionModel->saveCustomOption($sku, $productId);
-									}
-								}
-						}
-						Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('customoptions')->__('Option was successfully saved'));
-						Mage::getSingleton('adminhtml/session')->setFormData(false);
-						if ($this->getRequest()->getParam('back')) {
-							$this->_redirect('*/*/edit', array('id' => $optionId, 'sku' => $sku,"skulist"=>1));
-							return;
-						}
-						$this->_redirect('*/*/');
-						return;
-					} catch (Exception $e) {
-						Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-						Mage::getSingleton('adminhtml/session')->setFormData($data);
-						$this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
-						return;
-						}
-					}
-					else
-					{
-						 $this->_redirect('*/*/edit', array('id' => $optionId, 'sku' => $sku,"skulist"=>1));
-							return;
-					}
-		}
-		
+
+        $skurmv = str_replace(array("\r\n", "\r"), ",", $data["removesku"]["remove_sku"]);
+        $skurmvarray = explode(",", $skurmv);
+
+        if (count($skurmvarray) > 0 && !empty($skurmvarray[0])) {
+
+            try {
+                $optionModel = Mage::getModel("customoptions/customoptionscollection");
+                foreach ($skurmvarray as $rmsku) {
+
+                    if (!empty($rmsku)) {
+                        $product = Mage::getModel('catalog/product');
+                        $productId = $product->getIdBySku(trim($rmsku));
+                        if (!empty($productId)) {
+                            $optionModel->deleteCustomOption($sku, $productId);
+                        }
+                    }
+                }
+
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('customoptions')->__('Option was successfully saved'));
+                Mage::getSingleton('adminhtml/session')->setFormData(false);
+                $this->_redirect('*/*/');
+                return;
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                Mage::getSingleton('adminhtml/session')->setFormData($data);
+                $this->_redirect('*/*/adminhtml_remove/edit', array('id' => $this->getRequest()->getParam('id')));
+                return;
+            }
+        }
+        $this->_redirect('*/*/editsku', array('id' => $this->getRequest()->getParam('id')));
+        return;
+    }
+
+    public function saveAction()
+    {
+        $feature_id = Mage::getModel('catalog/product')->getIdBySku('customoptionmaster');
+
+        $optionId = $this->getRequest()->getParam("id");
+        $sku = Mage::getModel("catalog/product_option")->load($optionId)->getData("sku");
+
+        $sql = "select product_id from " . Mage::getConfig()->getTablePrefix() . "catalog_product_option where sku='$sku' and product_id != " . $feature_id;
+        $results = Mage::getSingleton('core/resource')->getConnection('core_read')->fetchAll($sql);
+        $categoryid = array();
+        $data = $this->getRequest()->getPost();
+
+        if (isset($data["productsku"])) {
+
+            $skuvalue = str_replace(array("\r\n", "\r"), ",", $data["productsku"]["sku_list"]);
+            $skuarray = explode(",", $skuvalue);
+            if (count($skuarray) > 0 && !empty($skuarray[0])) {
+                try {
+                    $optionModel = Mage::getModel("customoptions/customoptionscollection");
+                    foreach ($skuarray as $prodsku) {
+                        if (!empty($prodsku)) {
+                            $product = Mage::getModel('catalog/product');
+                            $productId = $product->getIdBySku(trim($prodsku));
+                            if (!empty($productId)) {
+                                $optionModel->saveCustomOption($sku, $productId);
+                            }
+                        }
+                    }
+                    Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('customoptions')->__('Option was successfully saved'));
+                    Mage::getSingleton('adminhtml/session')->setFormData(false);
+                    if ($this->getRequest()->getParam('back')) {
+                        $this->_redirect('*/*/edit', array('id' => $optionId, 'sku' => $sku, "skulist" => 1));
+                        return;
+                    }
+                    $this->_redirect('*/*/');
+                    return;
+                } catch (Exception $e) {
+                    Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                    Mage::getSingleton('adminhtml/session')->setFormData($data);
+                    $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+                    return;
+                }
+            }
+            else {
+                $this->_redirect('*/*/edit', array('id' => $optionId, 'sku' => $sku, "skulist" => 1));
+                return;
+            }
+        }
+
         if (isset($data['links'])) {
             $products = Mage::helper('adminhtml/js')->decodeGridSerializedInput($data['links']['products']);
         }
-		
+
         $productsId = array();
         $optionModel = Mage::getModel("customoptions/customoptionscollection");
         try {
@@ -140,13 +134,13 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
                         $optionModel->deleteCustomOption($sku, $result["product_id"]);
                     }
                 }
-            } 
+            }
             else {
-				
+
                 foreach ($results as $result) {
                     $optionModel->deleteCustomOption($sku, $result["product_id"]);
                 }
-                
+
             }
             Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('customoptions')->__('Option was successfully saved'));
             Mage::getSingleton('adminhtml/session')->setFormData(false);
@@ -164,23 +158,26 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         }
     }
 
-    public function productsAction() {
+    public function productsAction()
+    {
 
 
         $this->loadLayout();
         $this->getLayout()->getBlock('product.grid')
-                ->setSelectedProduct($this->getRequest()->getPost('products_id', null));
+            ->setSelectedProduct($this->getRequest()->getPost('products_id', null));
         $this->renderLayout();
     }
 
-    public function productsgridAction() {
+    public function productsgridAction()
+    {
         $this->loadLayout();
         $this->getLayout()->getBlock('product.grid')
-                ->setSelectedProduct($this->getRequest()->getPost('products_id', null));
+            ->setSelectedProduct($this->getRequest()->getPost('products_id', null));
         $this->renderLayout();
     }
 
-    public function deleteCategoryAction() {
+    public function deleteCategoryAction()
+    {
         $data = $this->getRequest()->getParams();
         $categoryid = $data["data"];
         $sku = $data["sku"];
@@ -203,7 +200,8 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         }
     }
 
-    public function saveCategoryAction() {
+    public function saveCategoryAction()
+    {
         $data = $this->getRequest()->getPost();
         $optionId = $this->getRequest()->getParam("id");
         $sku = Mage::getModel("catalog/product_option")->load($optionId)->getData("sku");
@@ -211,7 +209,8 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
 
             if (isset($data['categories'])) {
                 $this->_redirect('*/*/deletecategory', array('data' => implode(",", $data["categories"]), "id" => $optionId, "sku" => $sku));
-            } else {
+            }
+            else {
                 Mage::getSingleton('adminhtml/session')->addError(Mage::helper('customoptions')->__('Please select category'));
                 $this->_redirect('*/*/editcategory', array('id' => $optionId, 'sku' => $sku));
             }
@@ -242,7 +241,8 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         }
     }
 
-    public function editCategoryAction() {
+    public function editCategoryAction()
+    {
         $collection = Mage::getModel("catalog/product_option")->getCollection()->addFieldToFilter("main_table.option_id", $this->getRequest()->getParam("id"));
         $collection->getSelect()->join(Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title', 'main_table.option_id =' . Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title.option_id', array("title" => "title"))->group('title');
         Mage::register("customoption_data", $collection);
@@ -250,10 +250,12 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         $this->_setActiveMenu("customoptions/customoptions");
         $this->getLayout()->getBlock("head")->setCanLoadExtJs(true);
         $this->_addContent($this->getLayout()->createBlock("customoptions/adminhtml_categoryoptions_edit"))
-                ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_categoryoptions_edit_tabs"));
+            ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_categoryoptions_edit_tabs"));
         $this->renderLayout();
     }
-    public function editSkuAction() {
+
+    public function editSkuAction()
+    {
         Mage::unregister("customoption_data");
         $collection = Mage::getModel("catalog/product_option")->getCollection()->addFieldToFilter("main_table.option_id", $this->getRequest()->getParam("id"));
         $collection->getSelect()->join(Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title', 'main_table.option_id =' . Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title.option_id', array("title" => "title"))->group('title');
@@ -262,12 +264,13 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         $this->_setActiveMenu("customoptions/customoptions");
         $this->getLayout()->getBlock("head")->setCanLoadExtJs(true);
         $this->_addContent($this->getLayout()->createBlock("customoptions/adminhtml_removecustomoptions_edit"))
-                ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_removecustomoptions_edit_tabs"));
+            ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_removecustomoptions_edit_tabs"));
         $this->renderLayout();
     }
 
-    public function editAction() {
-		Mage::getSingleton("adminhtml/session")->addNotice("Your current setting for max_input_vars is ".ini_get("max_input_vars").". If you want to assign this custom option to more than ".ini_get("max_input_vars"). " products, then please increase this value in php.ini file, otherwise neglect this message.");
+    public function editAction()
+    {
+        Mage::getSingleton("adminhtml/session")->addNotice("Your current setting for max_input_vars is " . ini_get("max_input_vars") . ". If you want to assign this custom option to more than " . ini_get("max_input_vars") . " products, then please increase this value in php.ini file, otherwise neglect this message.");
         Mage::unregister("customoption_data");
         $collection = Mage::getModel("catalog/product_option")->getCollection()->addFieldToFilter("main_table.option_id", $this->getRequest()->getParam("id"));
         $collection->getSelect()->join(Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title', 'main_table.option_id =' . Mage::getConfig()->getTablePrefix() . 'catalog_product_option_title.option_id', array("title" => "title"))->group('title');
@@ -276,10 +279,9 @@ class Emipro_Customoptions_Adminhtml_IndexController extends Mage_Adminhtml_Cont
         $this->_setActiveMenu("customoptions/customoptions");
         $this->getLayout()->getBlock("head")->setCanLoadExtJs(true);
         $this->_addContent($this->getLayout()->createBlock("customoptions/adminhtml_customoptions_edit"))
-                ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_customoptions_edit_tabs"));
+            ->_addLeft($this->getLayout()->createBlock("customoptions/adminhtml_customoptions_edit_tabs"));
         $this->renderLayout();
     }
-    
-    
+
 
 }
